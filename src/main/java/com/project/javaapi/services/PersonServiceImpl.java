0d2deab6f2be0ef30a142b94a6344bfd53571199
Joao Kax <lukaxsilva.ls@gmail.com>
2023-02-03 15:52:58 -0300

    Comentários apagados

--- src/main/java/com/project/javaapi/services/PersonServiceImpl.java
+++ src/main/java/com/project/javaapi/services/PersonServiceImpl.java
@@ -1,6 +1,5 @@
 package com.project.javaapi.services;
 
-//import com.project.javaapi.data.PersonData;
 import com.project.javaapi.model.Person;
 import com.project.javaapi.model.RoleModel;
 import com.project.javaapi.repository.PersonRepository;
@@ -17,7 +16,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.Collection;
 import java.util.List;
-//import java.util.Optional;
 
 @Service
 @Transactional
@@ -33,16 +31,7 @@ public class PersonServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Person person = personRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
-
-//        Optional<Person> person = personRepository.findByUsername(username);
-//        if (person.isEmpty()) {
-//            throw new UsernameNotFoundException("Usuário [" + username + "] não encontrado");
-//        }
-
         return new User(person.getUsername(), person.getPassword(), true, true, true,true, getAuthority(person));
-
-//        return new User(person.get().getUsername(), person.get().getPassword(), true, true, true,true, person.get().getAuthorities());
-//        return new PersonData(person);
     }
 
     private Collection<? extends GrantedAuthority> getAuthority(Person person) {
