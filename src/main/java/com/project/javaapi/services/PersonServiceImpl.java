1c94336a73991944819c1abac8092bfe3158dba3
Joao Kax <lukaxsilva.ls@gmail.com>
2023-02-03 15:56:43 -0300

    Teste

--- src/main/java/com/project/javaapi/services/PersonServiceImpl.java
+++ src/main/java/com/project/javaapi/services/PersonServiceImpl.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.Collection;
 import java.util.List;
-
 @Service
 @Transactional
 public class PersonServiceImpl implements UserDetailsService {
